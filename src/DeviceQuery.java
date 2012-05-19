fba7baa8f2566246bc141cf17d898221e5026ffb
Cole Nelson <mail@colenelson.net>
2012-05-18 19:30:15 -0700

    deviceQuery classes, now requires JCUDA

---
+++ src/DeviceQuery.java
@@ -0,0 +1,193 @@
+/*
+ * Modified by nelsoncs for integration - 2012
+ * Team 4b: Michael Barger, Alex Kelly, Cole Nelson
+ * @author nelsoncs 2012-May-18 
+ *
+ */
+
+import static jcuda.driver.CUdevice_attribute.*;
+import static jcuda.driver.JCudaDriver.*;
+
+import java.util.*;
+
+import jcuda.driver.*;
+
+/**
+ * queries all attributes of all available devices
+ * uses JCUDA 
+ */
+public class DeviceQuery{
+	
+
+	private int deviceCount;
+	private Device [] cudaDevices;
+	
+    /**
+     * Constructor
+     */
+    public DeviceQuery()
+    {
+        JCudaDriver.setExceptionsEnabled(true);
+        cuInit(0);
+
+        // the number of devices
+    	int deviceCountArray[] = { 0 };
+
+        cuDeviceGetCount( deviceCountArray );
+        
+        this.deviceCount = deviceCountArray[0];
+        
+        // create array of device attribute objects
+        this.cudaDevices = new Device[this.deviceCount];
+
+        for (int i = 0; i < deviceCount; i++)
+        {
+        	cudaDevices[i] = new Device( i );
+        }
+    }
+	
+    /**
+	 * @return the deviceCount
+	 */
+	public int getDeviceCount() {
+		return deviceCount;
+	}
+
+	/**
+	 * @return the cudaDevices
+	 */
+	public Device[] getCudaDevices() {
+		return cudaDevices;
+	}
+
+    /**
+     * Returns a short description of the given CUdevice_attribute constant
+     * Copyright 2011 Marco Hutter - http://www.jcuda.org
+     * 
+     * @return A short description of the given constant
+     */
+    private static String getAttributeDescription(int attribute){
+    	
+        switch (attribute)
+        {
+            case CU_DEVICE_ATTRIBUTE_MAX_THREADS_PER_BLOCK: 
+                return "Maximum number of threads per block";
+            case CU_DEVICE_ATTRIBUTE_MAX_BLOCK_DIM_X: 
+                return "Maximum x-dimension of a block";
+            case CU_DEVICE_ATTRIBUTE_MAX_BLOCK_DIM_Y: 
+                return "Maximum y-dimension of a block";
+            case CU_DEVICE_ATTRIBUTE_MAX_BLOCK_DIM_Z: 
+                return "Maximum z-dimension of a block";
+            case CU_DEVICE_ATTRIBUTE_MAX_GRID_DIM_X: 
+                return "Maximum x-dimension of a grid";
+            case CU_DEVICE_ATTRIBUTE_MAX_GRID_DIM_Y: 
+                return "Maximum y-dimension of a grid";
+            case CU_DEVICE_ATTRIBUTE_MAX_GRID_DIM_Z: 
+                return "Maximum z-dimension of a grid";
+            case CU_DEVICE_ATTRIBUTE_MAX_SHARED_MEMORY_PER_BLOCK: 
+                return "Maximum shared memory per thread block in bytes";
+            case CU_DEVICE_ATTRIBUTE_TOTAL_CONSTANT_MEMORY: 
+                return "Total constant memory on the device in bytes";
+            case CU_DEVICE_ATTRIBUTE_WARP_SIZE: 
+                return "Warp size in threads";
+            case CU_DEVICE_ATTRIBUTE_MAX_PITCH: 
+                return "Maximum pitch in bytes allowed for memory copies";
+            case CU_DEVICE_ATTRIBUTE_MAX_REGISTERS_PER_BLOCK: 
+                return "Maximum number of 32-bit registers per thread block";
+            case CU_DEVICE_ATTRIBUTE_CLOCK_RATE: 
+                return "Clock frequency in kilohertz";
+            case CU_DEVICE_ATTRIBUTE_TEXTURE_ALIGNMENT: 
+                return "Alignment requirement";
+            case CU_DEVICE_ATTRIBUTE_MULTIPROCESSOR_COUNT: 
+                return "Number of multiprocessors on the device";
+            case CU_DEVICE_ATTRIBUTE_KERNEL_EXEC_TIMEOUT: 
+                return "Whether there is a run time limit on kernels";
+            case CU_DEVICE_ATTRIBUTE_INTEGRATED: 
+                return "Device is integrated with host memory";
+            case CU_DEVICE_ATTRIBUTE_CAN_MAP_HOST_MEMORY: 
+                return "Device can map host memory into CUDA address space";
+            case CU_DEVICE_ATTRIBUTE_COMPUTE_MODE: 
+                return "Compute mode";
+            case CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE1D_WIDTH: 
+                return "Maximum 1D texture width";
+            case CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_WIDTH: 
+                return "Maximum 2D texture width";
+            case CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_HEIGHT: 
+                return "aximum 2D texture height";
+            case CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE3D_WIDTH: 
+                return "Maximum 3D texture width";
+            case CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE3D_HEIGHT: 
+                return "aximum 3D texture height";
+            case CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE3D_DEPTH: 
+                return "Maximum 3D texture depth";
+            case CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_LAYERED_WIDTH: 
+                return "Maximum 2D layered texture width";
+            case CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_LAYERED_HEIGHT: 
+                return "Maximum 2D layered texture height";
+            case CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_LAYERED_LAYERS: 
+                return "Maximum layers in a 2D layered texture";
+            case CU_DEVICE_ATTRIBUTE_SURFACE_ALIGNMENT: 
+                return "Alignment requirement for surfaces";
+            case CU_DEVICE_ATTRIBUTE_CONCURRENT_KERNELS: 
+                return "Device can execute multiple kernels concurrently";
+            case CU_DEVICE_ATTRIBUTE_ECC_ENABLED: 
+                return "Device has ECC support enabled";
+            case CU_DEVICE_ATTRIBUTE_PCI_BUS_ID: 
+                return "PCI bus ID of the device";
+            case CU_DEVICE_ATTRIBUTE_PCI_DEVICE_ID: 
+                return "PCI device ID of the device";
+            case CU_DEVICE_ATTRIBUTE_TCC_DRIVER: 
+                return "Device is using TCC driver model";
+            case CU_DEVICE_ATTRIBUTE_MEMORY_CLOCK_RATE: 
+                return "Peak memory clock frequency in kilohertz";
+            case CU_DEVICE_ATTRIBUTE_GLOBAL_MEMORY_BUS_WIDTH: 
+                return "Global memory bus width in bits";
+            case CU_DEVICE_ATTRIBUTE_L2_CACHE_SIZE: 
+                return "Size of L2 cache in bytes";
+            case CU_DEVICE_ATTRIBUTE_MAX_THREADS_PER_MULTIPROCESSOR: 
+                return "Maximum resident threads per multiprocessor";
+            case CU_DEVICE_ATTRIBUTE_ASYNC_ENGINE_COUNT: 
+                return "Number of asynchronous engines";
+            case CU_DEVICE_ATTRIBUTE_UNIFIED_ADDRESSING: 
+                return "Device shares a unified address space with the host";
+            case CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE1D_LAYERED_WIDTH: 
+                return "Maximum 1D layered texture width";
+            case CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE1D_LAYERED_LAYERS: 
+                return "Maximum layers in a 1D layered texture";
+            case CU_DEVICE_ATTRIBUTE_PCI_DOMAIN_ID: 
+                return "PCI domain ID of the device";
+        }
+        return "(UNKNOWN ATTRIBUTE)";
+    }
+	
+	/**
+     * Unit test for this class
+     * TODO move to JUnit
+     * 
+     * @param args unused
+     */
+    public static void main(String args[])
+    {
+    	DeviceQuery query = new DeviceQuery();
+    	
+        System.out.println("Found " + query.deviceCount + " devices");
+
+        for (int i = 0; i < query.deviceCount; i++)
+        {
+            System.out.println("Device " + i + ": " 
+            							+ query.cudaDevices[i].getName() + 
+                " with Compute Capability " + query.cudaDevices[i].getMajor() 
+                + "." + query.cudaDevices[i].getMinor() );
+                    
+            for( Map.Entry<Integer, Integer> attribute : 
+            				query.cudaDevices[i].getAttributes().entrySet())
+            {   
+                System.out.println( getAttributeDescription( attribute.getKey() ) 
+                		+ " " + attribute.getValue() );
+
+            }
+        }
+    }
+
+
+}
